a1ad64579b765d18240f3db1cc1e97bf2289caad
Marshall Elford <melford@alumni.ubc.ca>
2018-11-26 10:36:54 -0800

    AlertController test and Travis.yml

--- tests/AlertController.test.js
+++ tests/AlertController.test.js
@@ -8,7 +8,19 @@ const request = require('supertest');
 const app = require('../app.js');
 
 describe('AlertController.js tests', function () {
-    it('post new alert', function () {
+    it('post new alert to all users in a companion session', function () {
+        request(app)
+            .post('/alert/1ffXuQT9yaUp1jO8CxZj')
+            .send({
+                "alertCode": 0
+            })
+            .end(function(err, res) {
+                expect(res.statusCode).toEqual(200);
+                done();
+            });
+    });
+    
+    it('post new alert to one user', function () {
         request(app)
             .post('/alert')
             .send({
